02ab4b7abc1a1d0175827c9d66a77973e67f6b53
KaterynaMarkevych <126855090+KaterynaMarkevych@users.noreply.github.com>
2024-02-29 01:48:07 +0200

    add some package and start header

---
+++ yumbox/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+      <header className='{styles.header}'>
+          <div>
+              <img scr="logo.png" alt="YumBox"/>
+          </div>
+
+    </header>
+  )
+}
+
+export default Header
