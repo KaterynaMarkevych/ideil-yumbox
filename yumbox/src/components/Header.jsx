360b0e6f7ad8a00f8b0ccb7cf02d34083ab71bb5
KaterynaMarkevych <126855090+KaterynaMarkevych@users.noreply.github.com>
2024-03-03 01:05:46 +0200

    working on header

--- yumbox/src/components/Header.jsx
+++ yumbox/src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import styled from 'styled-components';
+import { SlMenu } from "react-icons/sl";
 
 const Container = styled.div`
   padding-top: 20px;
   margin: 0 20px;
+  @media (min-width: 768px) {
+    padding-top: 25px;
+    margin: 0 30px;
+  }
+  @media (min-width: 1110px) {
+    padding-top: 32px;
+    margin: 0 82px;
+  }
 `;
 
 const HeaderContainer = styled.header`
@@ -11,8 +20,8 @@ const HeaderContainer = styled.header`
   text-align: center;
   justify-content: space-between;
   padding: 20px;
-  background-color: lightblue;
   color: #fff;
+  
 `;
 
 const Logo = styled.img`
@@ -22,9 +31,12 @@ const Logo = styled.img`
 `;
 
 const Navigation = styled.nav`
+  display: none;
+  @media (min-width: 768px) {
   display: flex;
+  align-items: center;
   justify-content: center;
-  margin-top: 20px;
+  }
 `;
 const List = styled.ul`
   list-style-type:none;
@@ -42,7 +54,64 @@ const Link = styled.a`
 `;
 
 const Basket = styled.button`
+  background: none;
+  color: #fff;
+  box-sizing: border-box;
+  border: 1px solid white;
+  border-radius: 21px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
   @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const BasketCounter = styled.span`
+  margin-left: 5px;
+  padding: 2px 6px;
+  border-radius: 10px;
+  background-color: #f00;
+  color: #fff;
+  font-size: 12px;
+`;
+
+const MenuButton = styled.button`
+  display: flex;
+  align-items: center;
+  background: none;
+  border: none;
+  cursor: pointer;
+  
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const MenuText = styled.p`
+position: absolute; 
+right: 70px; 
+color: rgb(255, 255, 255);
+  font-family: Montserrat;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 92%;
+  letter-spacing: 0%;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const InfoMenu = styled.button` 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: white;
+  @media (min-width: 768px) {
     display: none;
   }
 `;
@@ -50,8 +119,11 @@ const Header = () => {
   return (
     <Container>
     <HeaderContainer>
-        <Logo scr="logo.png" alt="YumBox"/>
-      <Navigation>
+        <Logo scr="logo.png" alt="YumBox" />
+        <MenuButton>
+          <MenuText>Меню</MenuText>
+        </MenuButton>
+        <Navigation>
         <List>
           <ListIteam>
             < Link href="/catalog">Каталог</Link>
@@ -67,8 +139,15 @@ const Header = () => {
           </ListIteam>
         </List>
         </Navigation>
-        <Basket>basket</Basket>
+        <Basket>
+          <BasketCounter>0</BasketCounter>
+          1234 грн
+        </Basket>
+        <InfoMenu>
+        <SlMenu />
+      </InfoMenu>
       </HeaderContainer>
+      
     </Container>
   )
 }
