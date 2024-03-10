1fe8825dc557935ce3cba1f35a846dfbc7bcb3a1
KaterynaMarkevych <katya.markevich73@gmail.com>
2024-03-10 23:38:40 +0200

    working on hero(not finished)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ import "swiper/css/pagination";
 import styled from "styled-components";
 import { Container } from "./Header";
 import heroImage from "../assets/images/Hero.png";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const HeroSwiper = styled(Swiper)`
   width: 100%;
@@ -17,7 +18,8 @@ const HeroSwiper = styled(Swiper)`
 `;
 const BoxSlides = styled.div`
   position: relative;
-  max-width: 1270px;
+  max-width: 100%;
+  height: 316px;
   border-radius: 20px;
   display: flex;
   align-items: center;
@@ -25,26 +27,55 @@ const BoxSlides = styled.div`
   flex-direction: column;
   z-index: 1;
   background: rgb(34, 34, 34);
+  @media (min-width: 769px){
+    max-width: 1276px;
+    height: 500px;
+    right: 50px;
+    left: 50px;
+  }
 `;
 const Image = styled.img`
   top: 0;
   z-index: 1;
   width: 300px;
   height: 280px;
-
+  @media (min-width: 769px){
+    width: 640px;
+    height: 455px;
+  }
 `;
 const Text = styled.p`
+  position: absolute;
+  top: -130%;
+  left: 50%;
+  color: rgb(233, 229, 225);
+  transform: translate(-50%, -50%);
   font-weight: 700;
   font-size: 100px;
-  color: rgb(233, 229, 225);
   text-align: center;
+  @media (min-width: 769px){
+    display: none;
+  }
+`;
+const DesktopText = styled.p`
+  position: absolute;
+  color: rgb(233, 229, 225);
+  font-weight: 600;
+  font-size: 220px;
+  left: 50%;
+  top: -280%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+@media (max-width: 769px){
+  display: none;
+}
 `;
 const HeroText = styled.div`
   display: flex;
-  align-items: center;
+  align-items: flex-end;
   justify-content: center;
   margin-top: 20px;
-  padding-bottom: 20px;
+  padding-bottom: 10px;
 `;
 const Text2 = styled.p`
   text-transform: uppercase;
@@ -55,36 +86,54 @@ const Text2 = styled.p`
 `;
 const HeroButton = styled.button`
   background: none;
-  border: none;
+  border: 3px solid rgb(252, 184, 82);
+  border-radius: 30px;
   cursor: pointer;
   text-transform: uppercase;
   font-size: 25px;
-  padding: 5px 34px; 
-  border: 3.px solid 
-  rgb(252, 184, 82);
-  border-radius: 17px;
+  padding: 5px 30px; 
   color: rgb(252, 184, 82);
+  margin-bottom: 25px;
 `;
 const HeroContent = styled.div`
-position: absolute;
-bottom: 0;
-margin-top: 80px;
-width: 100%;
-text-align: center;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
 `;
-/*position: absolute; margin-top: 80px; text-align: center; */ 
 const Hero = () => {
   return (
     <Container>
       <HeroSwiper
         modules={[Navigation, Pagination]}
-        navigation
+        navigation={{
+          nextEl: ".button-next-slide",
+          prevEl: ".button-prev-slide",
+          clickable: true,
+        }}
         pagination={{ clickable: true }}
         spaceBetween={30}
         slidesPerView={1}
       >
         <SwiperSlide key={1}>
           <BoxSlides>
+            <Image src={heroImage} alt="Hero Image" />
+            <HeroContent>
+            <Text>
+              YUM
+              <br />
+              BOX
+            </Text>
+            <DesktopText>YUMBOX</DesktopText>
+            <HeroText>
+              <Text2>сет 21</Text2>
+              <HeroButton>934 грн</HeroButton>
+            </HeroText>
+           </HeroContent>
+          </BoxSlides>
+        </SwiperSlide>
+        <SwiperSlide key={2}>
+        <BoxSlides>
             <Image src={heroImage} alt="Hero Image" />
             <HeroContent>
             <Text>
@@ -99,9 +148,30 @@ const Hero = () => {
            </HeroContent>
           </BoxSlides>
         </SwiperSlide>
-        <SwiperSlide key={2}>Slide 2</SwiperSlide>
-        <SwiperSlide key={3}>Slide 3</SwiperSlide>
+        <SwiperSlide key={3}>
+        <BoxSlides>
+            <Image src={heroImage} alt="Hero Image" />
+            <HeroContent>
+            <Text>
+              YUM
+              <br />
+              BOX
+            </Text>
+            <HeroText>
+              <Text2>сет 21</Text2>
+              <HeroButton>934 грн</HeroButton>
+            </HeroText>
+           </HeroContent>
+          </BoxSlides>
+        </SwiperSlide>
       </HeroSwiper>
+      <div className="button-next-slide">
+        <FaArrowRight size="15px" color="white" />
+      </div>
+      <div className="button-prev-slide">
+        <FaArrowLeft size="15px" color="white" />
+      </div>
+      <div className="pagination-wrap"></div>
     </Container>
   );
 };
